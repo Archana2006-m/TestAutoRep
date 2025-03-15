c8e0472230e97dc08dd3ba5df66bc513b5ec1b18
Archana2006-m <archana20.mani@gmail.com>
2025-03-16 08:14:17 +0900

    Update gumtree_page.py
    Modification for logging function

--- gumtree_page.py
+++ gumtree_page.py
@@ -1,21 +1,21 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from config import Config
-from config import logger
-
-
-
+from utils.logger import shared_logger
 class GumtreePage:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, Config.TIMEOUT)
-
     """ Confirm the title is displayed"""
     def validate_page(self, link):
+        shared_logger.info(f"Validating page...")
         self.driver.get(link)
         title = self.driver.title
-        logger.info(f"Title of the page: {title}")
+        shared_logger.info(f"Title of the page: {title}")
         assert title, "Title is not displayed"
+
+
 
         
