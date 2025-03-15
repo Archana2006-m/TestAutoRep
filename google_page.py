4091cc716672f920f315b2c85ec4fb011e28cb02
Archana2006-m <archana20.mani@gmail.com>
2025-03-16 08:13:30 +0900

    Update google_page.py
    Modification for logger

--- google_page.py
+++ google_page.py
@@ -4,6 +4,8 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from utils.logger import shared_logger
+
 
 class GooglePage:
     def __init__(self, driver):
@@ -14,6 +16,7 @@ class GooglePage:
         """Open the Google Page"""
         self.driver.get(Config.BASE_URL)
         self.wait.until(EC.presence_of_element_located((By.NAME, "q")))
+        shared_logger.info("Opening the Google Page")
 
     def search(self, query):
         """ Perform a search on Google."""
@@ -27,6 +30,7 @@ class GooglePage:
 
     def get_search_results(self):
         """Retrieve all search result elements."""
+        shared_logger.info("Retrieving the Google Search Results")
         return self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.g')))
 
     def extract_gumtree_links(self):
@@ -39,7 +43,7 @@ class GooglePage:
                 if Config.GUMTREE_DOMAIN in link:
                     gumtree_links.append(link)
             except Exception as e:
-                print(f"Error extracting link: {e}")
+                shared_logger.info(f"Error extracting link: {e}")
         return gumtree_links
 
     
