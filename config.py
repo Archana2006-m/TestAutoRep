8fd99d8c3996c3cf057cdde13046579f0d4c7421
Archana2006-m <archana20.mani@gmail.com>
2025-03-16 08:12:55 +0900

    Update config.py
    Modified the logging configuration

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
-import logging
-
-
-def configure_logging():
-    logging.basicConfig(level=logging.INFO)
-configure_logging()
-# Export the configured logger
-logger = logging.getLogger(__name__)
-
+import loggin
 class Config:
     BASE_URL = "https://www.google.com"
     SEARCH_TERM = "Cars in London"
